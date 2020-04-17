cd207fce724965d37b0372cd645ac8c28d4c4a8f
Annie Schmidt <aschmidt@pointblue.org>
2020-04-16 17:46:09 -0700

    small mods to adpe_col_var code

--- code/adpe_col_var.R
+++ code/adpe_col_var.R
@@ -8,11 +8,20 @@ d%>%
 
 
 # number of indiviudals in colonies with <3000
-d%>%
-  filter(Current.abundance<3000)%>%
-  summarise(n=n(),total=sum(Current.abundance), fraction=n/nrow(d)
+d%>%filter(Current.abundance<3000)%>%
+  summarise(n=n(),total=sum(Current.abundance), fraction=n/nrow(d))
+
+d%>%filter(!is.na(Current.abundance))%>%
+  summarise(n=n())
 # percent of world population in small colonies
 92984/sum(d$Current.abundance, na.rm=TRUE) # 0.0265292
+
+# percent of world population that is Crozier
+300000/sum(d$Current.abundance, na.rm=TRUE)
+
+300000/3790000
+100000/3800000
+
 
 summarise(d, total=sum(Current.abundance, na.rm=TRUE))
 
